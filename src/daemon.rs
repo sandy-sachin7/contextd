8c18021611e37fd339f967e2f9f34506a9e6fdf7
SANTHOSH-SACHIN <sandy42107@gmail.com>
2025-12-15 19:40:27 +0530

    core: Initialize project structure and dependencies

---
+++ src/daemon.rs
@@ -0,0 +1,5 @@
+pub async fn run() -> anyhow::Result<()> {
+    println!("Daemon running");
+    // TODO: Initialize components
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod api;
+mod config;
+mod daemon;
+mod indexer;
+mod storage;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    println!("contextd starting...");
+    daemon::run().await
+}
