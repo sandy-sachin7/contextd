5b9d0258cadf1b48f7547493f9251e5e0086ba30
SANTHOSH-SACHIN <sandy42107@gmail.com>
2025-12-15 21:51:24 +0530

    refactor: remove unused `Watcher` import from `notify_debouncer_mini`

--- src/indexer/watcher.rs
+++ src/indexer/watcher.rs
@@ -1,4 +1,4 @@
-use notify_debouncer_mini::notify::{self, RecursiveMode, Watcher};
+use notify_debouncer_mini::notify::{self, RecursiveMode};
 use notify_debouncer_mini::{new_debouncer, DebounceEventResult, Debouncer};
 use std::path::PathBuf;
 use std::sync::mpsc::Sender;
@@ -12,7 +12,9 @@ pub fn watch(
     let mut debouncer = new_debouncer(Duration::from_millis(2000), tx)?;
 
     for path in paths {
-        debouncer.watcher().watch(path, RecursiveMode::Recursive)?;
+        debouncer
+            .watcher()
+            .watch(path, RecursiveMode::Recursive)?;
     }
 
     Ok(debouncer)
